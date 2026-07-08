efb8205ecb032ee345201d4a20243881adc8bde1
Venkat Rajesh <venkatrajesh570@gmail.com>
2026-07-09 02:27:32 +0530

    fix: resolve ambiguous token namespace reference breaking container build

--- New Floder/WebApplication1/Controllers/AuthController.cs
+++ New Floder/WebApplication1/Controllers/AuthController.cs
@@ -1,13 +1,10 @@
 ﻿using Google.Apis.Auth;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 using WebApplication1.Models;
 
 namespace WebApplication.Controllers
